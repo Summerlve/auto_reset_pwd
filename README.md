beac238571974b89f457ea87c3b85b588997f2f8
Summerlve <dzxx0563zh@126.com>
2022-02-21 12:43:16 +0800

    load account info

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # auto_reset_pwd
 auto reset stu's pwd of suzhou snd edu system
+
+create a file <account.json> in project folder and type in:
+```javascript
+{
+    "username": "",
+    "password": ""
+}
+```
+fill in your account information into username and password field for website auth.
+
+install dep first:
+```bash
+pip3 install selenium
+pip3 install webdriver-manager
+```
+
+usage:
+```bash
+python3 ./auto_reset_pwd.py
+```
--- auto_reset_pwd.py
+++ auto_reset_pwd.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from os import path
+from multiprocessing.dummy import current_process
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from selenium.webdriver.common.by import By
+import pathlib
+import os
+import json
 
-service = ChromeService(executable_path = ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
-driver.get("https://jssnd.edu.cn/manager.html")
-sleep(5)
-driver.quit()
+# load account info
+cur_path = pathlib.Path(__file__).parent.resolve()
+account_json_path = os.path.join(cur_path, "account.json")
+print(account_json_path)
+with open(account_json_path, "r") as file:
+    account_data = json.load(file)
+print(account_data)
+
+# service = ChromeService(executable_path = ChromeDriverManager().install())
+# driver = webdriver.Chrome(service=service)
+# driver.get("https://jssnd.edu.cn/login.html")
+
+# username_ele = driver.find_element(By.ID, "user")
+# password_ele = driver.find_element(By.ID, "login_psd") 
+
+# username_ele.send_keys("test")
+
+# sleep(5)
+# driver.quit()
